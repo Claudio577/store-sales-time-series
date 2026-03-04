27a710895ca4e2feedf01ab8e5fc49bbb661c307
Claudio577 <chideki.yoshida@gmail.com>
2026-03-04 09:55:30 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.title("📊 Store Sales Predictor")
+
+model = pickle.load(open("model/model.pkl", "rb"))
+
+st.sidebar.header("Input Data")
+
+store = st.sidebar.number_input("Store Number", 1, 54, 1)
+family = st.sidebar.number_input("Product Family Code", 0, 30, 1)
+onpromotion = st.sidebar.number_input("Promotion Items", 0, 100, 0)
+
+date = st.sidebar.date_input("Select Date")
+
+year = date.year
+month = date.month
+day = date.day
+dayofweek = date.weekday()
+
+input_data = pd.DataFrame({
+    "store_nbr":[store],
+    "family":[family],
+    "year":[year],
+    "month":[month],
+    "day":[day],
+    "dayofweek":[dayofweek],
+    "onpromotion":[onpromotion]
+})
+
+prediction = model.predict(input_data)
+
+st.subheader("Predicted Sales")
+
+st.write(f"Estimated sales: {prediction[0]:.2f}")
